86832113d98ccfd45a21a381dee425dcf03d8bee
sudar1977 <sudar1977@gmail.com>
2023-03-10 13:39:08 +0300

    Shader

---
+++ Assets/Code/Configs/CameraSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Code.Configs
+{
+    [CreateAssetMenu(fileName = "CameraSettings", menuName = "Data/CameraSettings", order = 0)]
+    public class CameraSettings : ScriptableObject
+    {
+        public Transform StartPosition;
+        
+    }
+}
